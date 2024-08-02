f83032e6430f8f3b786616120f024273fe53101c
Eric Coissac <eric@coissac.eu>
2024-08-02 14:30:53 +0200

    Tag version

--- pkg/obioptions/version.go
+++ pkg/obioptions/version.go
@@ -7,8 +7,8 @@ import (
 // TODO: The version number is extracted from git. This induces that the version
 // corresponds to the last commit, and not the one when the file will be
 // commited
-var _Commit = "1b1cd41"
-var _Version = ""
+var _Commit = "c0c1803"
+var _Version = "Release 4.2.0"
 
 // Version returns the version of the obitools package.
 //
